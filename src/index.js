8060932875ae8f61bb881dfda6d8e5ff1da8ff7a
Fedor Parenkov <parenkov.fedor@gmail.com>
2022-07-31 20:56:24 +0400

    feat: add function that decodes morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const encodedLetters = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        encodedLetters.push(expr.substring(i, i + 10));
+    }
+
+    for (let i = 0; i < encodedLetters.length; i++) {
+        encodedLetters.splice(i, 1, encodedLetters[i].substring(encodedLetters[i].indexOf('1')));
+    }
+
+    const morseCode = [];
+
+    encodedLetters.forEach((element) => {
+        const tempStorage = [];
+
+        for (let i = 1; i < element.length; i += 2) {
+            if (element[i] === '*') {
+                tempStorage.push('**');
+            } else {
+                if (element[i] === '0') {
+                    tempStorage.push('.');
+                } else {
+                    tempStorage.push('-');
+                }
+            }
+        }
+        morseCode.push(tempStorage.join(''));
+    });
+
+    for (let i = 0; i < morseCode.length; i++) {
+        if (morseCode[i] === '**********') {
+            morseCode.splice(i, 1, ' ');
+        } else {
+            morseCode.splice(i, 1, MORSE_TABLE[morseCode[i]]);
+        }
+    }
+    return morseCode.join('');
 }
 
 module.exports = {
